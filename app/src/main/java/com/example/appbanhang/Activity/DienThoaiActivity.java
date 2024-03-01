28c8d9c8555f9f6e54b207080abbbd77508ce213
Si1ly <102406756+Si1ly@users.noreply.github.com>
2024-03-01 15:53:46 +0700

    Video 14 + video 15 ( chưa làm nhưng hiểu hết )

--- app/src/main/java/com/example/appbanhang/Activity/DienThoaiActivity.java
+++ app/src/main/java/com/example/appbanhang/Activity/DienThoaiActivity.java
@@ -1,25 +1,82 @@
 package com.example.appbanhang.Activity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.appbanhang.Adapter.DienThoaiAdapter;
+import com.example.appbanhang.Model.SanPhamMoi;
 import com.example.appbanhang.R;
+
+import java.util.List;
 
 public class DienThoaiActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     RecyclerView recyclerView;
+    int page = 1;
+    int loai;
+    DienThoaiAdapter dienThoaiAdapter;
+    List<SanPhamMoi> sanPhamMoiList;
+    LinearLayoutManager linearLayoutManager;
+    Handler handler = new Handler();
+    boolean isLoading = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dienthoai);
         link();
         ActionBar();
+        getData();
+        addEventload();
+
+    }
+
+    private void addEventload() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if(isLoading == false){
+                    if(linearLayoutManager.findLastCompletelyVisibleItemPosition()==sanPhamMoiList.size()-1){
+                        isLoading = true;
+                        loadMore();
+                    }
+                }
+            }
+        });
+    }
+
+    private void loadMore() {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                sanPhamMoiList.add(null);
+                dienThoaiAdapter.notifyItemInserted(sanPhamMoiList.size()-1);
+            }
+        });
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                sanPhamMoiList.remove(sanPhamMoiList.size()-1);
+                dienThoaiAdapter.notifyItemRemoved(sanPhamMoiList.size());
+                page = page +1;
+            }
+        },2000);
+    }
+
+    private void getData() {
     }
 
     private void ActionBar() {
@@ -31,8 +88,9 @@ public class DienThoaiActivity extends AppCompatActivity {
     private void link(){
         toolbar = findViewById(R.id.toolbar);
         recyclerView = findViewById(R.id.recycleView_dt);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
     }
 }
--- app/src/main/java/com/example/appbanhang/Adapter/DienThoaiAdapter.java
+++ app/src/main/java/com/example/appbanhang/Adapter/DienThoaiAdapter.java
@@ -1,6 +1,7 @@
 package com.example.appbanhang.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +13,15 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.appbanhang.Activity.ChiTietActivity;
+import com.example.appbanhang.InterfaceUsing.ItemClickListener;
 import com.example.appbanhang.Model.SanPhamMoi;
 import com.example.appbanhang.R;
 
 import java.text.DecimalFormat;
 import java.util.List;
 
-public class DienThoaiAdapter extends RecyclerView.Adapter<DienThoaiAdapter.DienThoaiViewHolder> {
+public class DienThoaiAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private static final int VIEW_TYPE_DATA = 0;
     private static final int VIEW_TYPE_LOADING = 1;
@@ -32,19 +35,45 @@ public class DienThoaiAdapter extends RecyclerView.Adapter<DienThoaiAdapter.Dien
 
     @NonNull
     @Override
-    public DienThoaiViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dienthoai,parent,false);
-        return new DienThoaiViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_DATA) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dienthoai, parent, false);
+            return new DienThoaiViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading, parent, false);
+            return new LoadingViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull DienThoaiViewHolder holder, int position) {
-        SanPhamMoi sanPhamMoi = new SanPhamMoi();
-        holder.ten.setText(sanPhamMoi.getTensp());
-        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
-        holder.gia.setText("price"+decimalFormat.format(Double.parseDouble(sanPhamMoi.getGiasp())));
-        holder.mota.setText(sanPhamMoi.getMota());
-        Glide.with(context).load(sanPhamMoi.getHinhanh()).into(holder.hinhanh);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof DienThoaiViewHolder) {
+            DienThoaiViewHolder dienThoaiViewHolder = (DienThoaiViewHolder) holder;
+            SanPhamMoi sanPhamMoi = array.get(position);
+            dienThoaiViewHolder.ten.setText(sanPhamMoi.getTensp());
+            DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+            dienThoaiViewHolder.gia.setText("price" + decimalFormat.format(Double.parseDouble(sanPhamMoi.getGiasp())));
+            dienThoaiViewHolder.mota.setText(sanPhamMoi.getMota());
+            Glide.with(context).load(sanPhamMoi.getHinhanh()).into(dienThoaiViewHolder.hinhanh);
+            dienThoaiViewHolder.setItemClickListener(new ItemClickListener() {
+                @Override
+                public void onClick(View view, int pos, boolean isLongClick) {
+                    if(!isLongClick){
+                        Intent i = new Intent(context, ChiTietActivity.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        context.startActivity(i);
+                    }
+                }
+            });
+        } else {
+            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
+            loadingViewHolder.progressBar.setIndeterminate(true);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return array.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_DATA;
     }
 
     @Override
@@ -52,7 +81,7 @@ public class DienThoaiAdapter extends RecyclerView.Adapter<DienThoaiAdapter.Dien
         return array.size();
     }
 
-    public class LoadingViewHolder extends RecyclerView.ViewHolder{
+    public class LoadingViewHolder extends RecyclerView.ViewHolder {
         ProgressBar progressBar;
 
         public LoadingViewHolder(@NonNull View itemView) {
@@ -61,10 +90,11 @@ public class DienThoaiAdapter extends RecyclerView.Adapter<DienThoaiAdapter.Dien
         }
     }
 
-    public class DienThoaiViewHolder extends RecyclerView.ViewHolder {
+    public class DienThoaiViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        TextView ten,gia,mota;
+        TextView ten, gia, mota;
         ImageView hinhanh;
+        private ItemClickListener itemClickListener;
 
         public DienThoaiViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -72,6 +102,15 @@ public class DienThoaiAdapter extends RecyclerView.Adapter<DienThoaiAdapter.Dien
             gia = itemView.findViewById(R.id.gia_dt);
             mota = itemView.findViewById(R.id.mota_dt);
             hinhanh = itemView.findViewById(R.id.anh_dt);
+            itemView.setOnClickListener(this);
+        }
+
+        public void setItemClickListener(ItemClickListener itemClickListener){
+            this.itemClickListener = itemClickListener;
+        }
+        @Override
+        public void onClick(View view) {
+            itemClickListener.onClick(view,getAdapterPosition(),false);
         }
     }
 }
