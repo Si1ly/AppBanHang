1b9a9c3e0aeb1d43e95df149972f7df6a4e454ae
Si1ly <102406756+Si1ly@users.noreply.github.com>
2024-03-11 23:48:24 +0700

    Phần code đăng nhập với đăng kí

--- app/src/main/java/com/example/appbanhang/Activity/DangNhapActivity.java
+++ app/src/main/java/com/example/appbanhang/Activity/DangNhapActivity.java
@@ -1,5 +1,6 @@
 package com.example.appbanhang.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
@@ -7,11 +8,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.appbanhang.R;
 import com.example.appbanhang.Retrofit.ApiBanHang;
 import com.example.appbanhang.Retrofit.RetrofitClient;
 import com.example.appbanhang.Utils.Utils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
@@ -21,6 +27,7 @@ public class DangNhapActivity extends AppCompatActivity {
     EditText edt_email_login,edt_pass_login;
     AppCompatButton button;
     ApiBanHang apiBanHang;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
@@ -39,7 +46,16 @@ public class DangNhapActivity extends AppCompatActivity {
         button.setOnClickListener(task1->{
             String email = edt_email_login.getText().toString().trim();
             String pass = edt_pass_login.getText().toString().trim();
-            //Api
+            //Api cho phần Server
+            
+            firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(DangNhapActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         });
     }
 
